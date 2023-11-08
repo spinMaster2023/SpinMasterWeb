a8f6dbfee4d401e27cc7ef3852f8e0551f90ba36
309 <309@academicos.cesde.edu.co>
2023-11-07 20:47:02 -0500

    arreglo de tarjetas

--- src/Noticias/noticias.jsx
+++ src/Noticias/noticias.jsx
@@ -1,65 +1,70 @@
 import './style.css'
 export function Noticias() {
+
     return (
         <>
-            <div className="container my-4 ">
-                <div className="row ">
-                    
-                    <div className="col-4">
-                        <div class="card-container">
-                            <div class="card">
-                                <div class="img-content">
+            <div className="container my-4">
+                <div className="row">
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <div className="card-container">
+                            <div className="card">
+                                <div className="img-content">
                                     <img src="https://img.freepik.com/fotos-premium/raqueta-tenis-llamas-sobre-fondo-llama-oscura_843481-944.jpg" alt="" />
                                 </div>
-                                <div class="content">
-                                    <p class="heading">Card Hover</p>
+                                <div className="content">
+                                    <p className="heading">Card Hover</p>
                                     <p>
                                         Lorem ipsum dolor sit amet, consectetur adipii
                                         voluptas ten mollitia pariatur odit, ab
                                         minus ratione adipisci accusamus vel est excepturi laboriosam magnam
                                         necessitatibus dignissimos molestias.
                                     </p>
-                                    <button type="button" class="btn btn-dark">VER MAS</button>
+                                    <button type="button" className="btn btn-dark">VER MAS</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="col-4"> <div class="card-container">
-                            <div class="card">
-                                <div class="img-content">
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <div className="card-container">
+                            <div className="card">
+                                <div className="img-content">
                                     <img src="https://img.freepik.com/fotos-premium/raqueta-tenis-llamas-sobre-fondo-llama-oscura_843481-944.jpg" alt="" />
                                 </div>
-                                <div class="content">
-                                    <p class="heading">Card Hover</p>
+                                <div className="content">
+                                    <p className="heading">Card Hover</p>
                                     <p>
                                         Lorem ipsum dolor sit amet, consectetur adipii
                                         voluptas ten mollitia pariatur odit, ab
                                         minus ratione adipisci accusamus vel est excepturi laboriosam magnam
                                         necessitatibus dignissimos molestias.
                                     </p>
-                                    <button type="button" class="btn btn-dark">VER MAS</button>
+                                    <button type="button" className="btn btn-dark">VER MAS</button>
                                 </div>
                             </div>
-                        </div> </div>
-                    <div className="col-4"> <div class="card-container">
-                            <div class="card">
-                                <div class="img-content">
+                        </div>
+                    </div>
+                    <div className="col-12 col-sm-6 col-md-4">
+                        <div className="card-container">
+                            <div className="card">
+                                <div className="img-content">
                                     <img src="https://img.freepik.com/fotos-premium/raqueta-tenis-llamas-sobre-fondo-llama-oscura_843481-944.jpg" alt="" />
                                 </div>
-                                <div class="content">
-                                    <p class="heading">Card Hover</p>
+                                <div className="content">
+                                    <p className="heading">Card Hover</p>
                                     <p>
                                         Lorem ipsum dolor sit amet, consectetur adipii
                                         voluptas ten mollitia pariatur odit, ab
-                                        minus ratione adipisci accusamus vel est excepturi laboriosam magnam
-                                        necessitatibus dignissimos molestias.
+                                        minus ratione adipisci accusamus vel est excepturi
                                     </p>
-                                    <button type="button" class="btn btn-dark">VER MAS</button>
+                                    <button type="button" className="btn btn-dark">VER MAS</button>
                                 </div>
                             </div>
-                        </div> </div>
+                        </div>
+                    </div>
                 </div>
             </div>
+
+            <button type="button" className="btn btn-dark">VER MAS</button>
 
 
         </>
