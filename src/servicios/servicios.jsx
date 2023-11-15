267beb9bfb318d845d60e7860ea8dcc968d007cd
Juan diego <ranchermessi@gmail.com>
2023-11-15 17:02:59 -0500

    query

--- src/servicios/servicios.jsx
+++ src/servicios/servicios.jsx
@@ -8,22 +8,50 @@ export function Services() {
         <div className="container-fluid ">
           <div className="row">
             <div className="col-12">
-          
-              
               <div className="contenedor">
                 <div className="item item-1 m-2 "><img src="../../src/assets/img/jugador.png" alt="" className='principal' /></div>
-                <div className="item item-2 "><img src="../../src/assets/img/chop.jpg" alt="" className=' imagenes ' /></div>
-                <div className="item item-3 "><img src="../../src/assets/img/chop.jpg" alt="" className='imagenes ' /></div>
-                <div className="item item-4 "><img src="../../src/assets/img/chop.jpg" alt="" className=' imagenes ' /></div>
-                <div className="item-5 "><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium nihil necessitatibus tempora quasi doloremque sunt labore quo, iste reprehenderit id totam velit corrupti iusto? Possimus voluptas id atque pariatur iste.
-                Alias repellat quo aliquid laudantium ipsa minima itaque cupiditate dolore rem voluptatum soluta qui hic, maxime asperiores doloribus, reiciendis possimus beatae explicabo voluptas voluptate illum! Tenetur ipsam commodi mollitia unde!</p></div>
-                <div className="item-6 "><button className='btn btn-warning fs-5'> Registrate!</button></div>
+                <div className="item item-2 "><img src="../../src/assets/img/saques.png" alt="" className=' imagenes ' /></div>
+                <div className="item item-3 "><img src="../../src/assets/img/posicion.png" alt="" className='imagenes ' /></div>
+                <div className="item item-4 "><img src="../../src/assets/img/tecnicas.png" alt="" className=' imagenes ' /></div>
+                <div className="item-5 "><p>En nuestra página, te sumergirás en el fascinante mundo del ping pong, aprendiendo desde los fundamentos esenciales hasta las técnicas avanzadas. Descubrirás la perfección en la ejecución de saques, dominarás las posiciones estratégicas en la mesa y adquirirás habilidades técnicas que te convertirán en un jugador de ping pong más hábil y competitivo. ¡Prepárate para elevar tu juego con nosotros!</p></div>
+                <div className="item-6 "><button className='btn btn-warning fs-5 botonic'> Registrate!</button></div>
               </div>
             </div>
           </div>
         </div>
       </div>
-                            
+
+
+
+      <div className="container-fluid cont2">
+        <div className="row">
+          <div className="col-12">
+
+            <div className="contenedor2">
+              <div className="item item-1 m-2">
+              <p>En nuestra página, te sumergirás en el fascinante mundo del ping pong, aprendiendo desde los fundamentos esenciales hasta las técnicas avanzadas. Descubrirás la perfección en la ejecución de saques, dominarás las posiciones estratégicas en la mesa y adquirirás habilidades técnicas que te convertirán en un jugador de ping pong más hábil y competitivo. ¡Prepárate para elevar tu juego con nosotros!</p>
+              <button className='btn btn-warning fs-5 botonic'>Registrate!</button>
+              </div>
+              
+                
+              
+              <div className="item item-2 col-12 col-md-6 col-lg-4">
+                <img src="../../src/assets/img/saques.png" alt="" className='imagenes' />
+              </div>
+              <div className="item item-3 col-12 col-md-6 col-lg-4">
+                <img src="../../src/assets/img/posicion.png" alt="" className='imagenes' />
+              </div>
+              <div className="item item-4 col-12 col-md-6 col-lg-4">
+                <img src="../../src/assets/img/tecnicas.png" alt="" className='imagenes' />
+              </div>
+
+              
+            </div>
+          </div>
+        </div>
+      </div>
+
+
 
     </>
   )
